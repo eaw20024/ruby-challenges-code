1868bcdd5595e6e007159b908abd4293ede893d5
eaw5117 <eric.washington.me@gmail.com>
2015-10-14 20:42:40 +0000

    Added sinatra gem to numerology app

--- numerology.rb
+++ numerology.rb
@@ -1,50 +1,70 @@
+require 'sinatra'
+
+set :bind, '0.0.0.0'
 
 def get_birth_path_num(birthdate)
-    number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
-    number = number.to_s
-    number = number[0].to_i + number[1].to_i
-    
-    if number > 9 
-    number[0].to_i + number[1].to_i
-    end
-    
-    return number
-end
+		number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
 
-def get_message(number)
+		number = number.to_s
+		number = number[0].to_i + number[1].to_i
 
-    case(number)
-    when 1 
-    puts "Your number is #{number} and you're the leader. The number #{number} indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
-    when 2
-    puts "Your number is #{number} and you're the mediator and peace-lover. The number #{number} indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
-    when 3
-    puts "Your number is #{number} and you're sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three’s enjoy life and have a good sense of humor. Ruled by Jupiter."
-    when 4
-    puts "Your number is #{number} and you're the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
-    when 5
-    puts "Your number is #{number} and you're freedom lover. The number #{number} is an intellectual vibration. These are ‘idea’ people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
-    when 6
-    puts "Your number is #{number} and you're the peace lover. The number #{number} is a loving, stable, and harmonious vibration. Ruled by Venus."
-    when 7
-    puts "Your number is #{number} and you're the deep thinker. The number #{number} is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
-    when 8
-    puts "Your number is #{number} and you're the manager. Number #{number} is a strong, successful, and material vibration. Ruled by Saturn."
-    when 9
-    puts "Your number is #{number} and you're the teacher. Number #{number} is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
-    else
-    puts "#{name}, Uh oh! Your birth path number is not 1-9!"
-    end
-end
+		if number > 9
+			number = number[0].to_i + number[1].to_i
+		end
 
-puts "What is your birthdate? Please write it in this format: MMDDYYYY"
-birthdate = gets
+		return number
+	end
 
-birth_path_num = get_birth_path_num(birthdate)
+	def get_message(birth_path_number)
 
-message = get_message(birth_path_num)
+		case birth_path_number
+			
+		when 1
+            message = "Your numerology number is #{birth_path_number}\nOne is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
+		when 2
+			message = "Your numerology number is #{birth_path_number}\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
 
-puts message
+		when 3
+			message = "Your numerology number is #{birth_path_number}\nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three’s enjoy life and have a good sense of humor. Ruled by Jupiter."
+
+		when 4
+			message = "Your numerology number is #{birth_path_number}\nThis is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
+
+		when 5
+			message = "Your numerology number is #{birth_path_number}\nThis is the freedom lover. The number five is an intellectual vibration. These are ‘idea’ people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+
+		when 6
+			message = "Your numerology number is #{birth_path_number}\nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
+
+		when 7
+			message = "Your numerology number is #{birth_path_number}\nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
+
+		when 8
+			message = "Your numerology number is #{birth_path_number}\nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
+
+		when 9
+			message = "Your numerology number is #{birth_path_number}\nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+
+		else 
+			message = "Uh oh! Your birth path number is not between 1-9!"
+		end
+	end
+
+	puts "What is your birthdate? Please write in in this format: MMDDYYYY"
+	birthdate = gets
+
+	birth_path_number = get_birth_path_num(birthdate)
+
+	message = get_message(birth_path_number)
+
+	puts message
+
+	get '/:birthdate' do
+	birthdate = params[:birthdate]	
+	birth_path_number = get_birth_path_num(birthdate)
+	message = get_message(birth_path_number)
+	"#{message}"
+	end
 
 
     
